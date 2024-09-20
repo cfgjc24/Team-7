5982542b8abd07ada610869a85eb11f0af8031e9
taisnguyen <tai.sanh.nguyen@fusionsystems.co.jp>
2024-09-20 14:16:26 -0400

    fixed template

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,3 @@
 from flask import Flask
 
 app = Flask(__name__, static_url_path="", static_folder="../build")
-
-app.config.from_object("app.config")
-app.secret_key = app.config["SECRET_KEY"]
-
-from app import http
