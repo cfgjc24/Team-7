d75e6cbd681aff24100c3e5ff5eed39311c3f157
Shriya Mahakala <shriya.mahakala@gmail.com>
2024-09-21 03:52:36 -0400

    fixing bugs

--- app/mongo.py
+++ app/mongo.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
-from app.db import db
+from db import db
 
 
 sessions = db["sessions"]
@@ -10,18 +10,18 @@ caregivers = db["caregivers"]
 #if the caregiverID is already in the database, update the entry. Otherwise, insert a new entry
 def save(caregiverID, state, geodata, client, timestamp, active, alert):
     for session in sessions.find():
-        if session.get('caregiverID', "") == caregiverID:
-            sessions.update_one({'caregiverID': caregiverID, 'state': state, 'geodata': geodata, 'client': client, 'timestamp': timestamp, "active": active, "alert": alert})
+        if session.get('caregiverID', "") == caregiverID and session.get('active', False):
+            sessions.update_one({'_ID': session.get("_ID")}, {'$set': {'caregiverID': caregiverID, 'state': state, 'geodata': geodata, 'client': client, 'timestamp': timestamp, "active": active, "alert": alert}})
             return
     sessions.insert_one({'caregiverID': caregiverID, 'state': state, 'geodata': geodata, 'client': client, 'timestamp': timestamp, "active": active, "alert": alert})
 
-#returns all active users and all active users that have been alerted
+#returns all active users and all active users that are alerting
 def get():
-    activeUsers = []
-    alertedUsers = []
+    activeUsers = [] #all active users
+    alertedUsers = [] #all active users that are alerting
     for session in sessions.find():
         if session.get('active', ""):
-            activeUsers.append(session)
+            activeUsers.append(session.copy())
             
             caregiverDict = caregivers.find_one({'caregiverID': session['caregiverID']})
             if caregiverDict:
@@ -43,7 +43,7 @@ def getByCaregiverID(caregiverID):
     res = [None]
     for session in sessions.find():
         if session.get('caregiverID', "") == caregiverID and session.get('active', False):
-            res[0]=session
+            res[0]=session.copy()
             
     if not res[0]:
         return {"error": "Active Caregiver not found"}
@@ -58,18 +58,24 @@ def getByCaregiverID(caregiverID):
     
 #doesnt actually remove. just inactivates a certain field
 def remove(caregiverID):
-    result = sessions.update_one({'caregiverID': caregiverID}, {'$set': {'active': False}})
+    for session in sessions.find():
+        if session.get('caregiverID', "") == caregiverID and session.get('active', False):
+            sessions.update_one({'_ID': session.get("_ID")}, {'$set': {'active': False}})
+            return {"status": "Caregiver removed"}
+    return {"status": "Caregiver not found"}
 
 #Toggle the alert field for the caregiverID
 def alert(caregiverID):
-    session = sessions.find_one({'caregiverID': caregiverID})
-    if session and session.get('active', False):
-        alert_value = not session.get('alert', False)
-        sessions.update_one({'caregiverID': caregiverID}, {'$set': {'alert': alert_value}})
-        return {"success": "Alert toggled successfully"}
-    else:
-        return {"error": "Caregiver not found"}
+    res = getByCaregiverID(caregiverID)
+    if "error" in res:
+        return res
+    sessions.update_one({'caregiverID': caregiverID}, {'$set': {'alert': False}})
+    return res
 
+
+
+print(remove('123'))
+print(get())
 
 
 
