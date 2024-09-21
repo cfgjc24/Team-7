1661727f488462c0a2673a2501c7a8a454ddbd41
Arielle Dong <112361805+Arielle8219@users.noreply.github.com>
2024-09-20 20:10:57 -0400

    render provider list

--- src/pages/ProviderMap/supervisor.js
+++ src/pages/ProviderMap/supervisor.js
@@ -1,18 +1,48 @@
 import React, { useState, useEffect } from "react";
 import ProviderMap from "./map";
-import { Container, Box } from "@mui/material";
+import {
+  Container,
+  Box,
+  TextField,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import Grid from "@mui/material/Grid2";
 const Supervisor = () => {
+  const [providerList, setProviderList] = useState([]);
+
+  useEffect(() => {
+    setProviderList(["provider 1", "provider 2"]);
+  }, []);
+
   return (
-    <Grid container spacing={3}>
-      <Grid item xs style={{ flexGrow: 1 }}>
-        <p>Alert</p>
+    <Grid container>
+      <Grid item xs style={{ flexGrow: 0.5 }}>
+        <p>Find Provider</p>
+        <TextField
+          id="search-provider"
+          label="Search by name"
+          variant="outlined"
+        />
+        <hr></hr>
+        <List>
+          {providerList.map((provider, index) => (
+            <React.Fragment key={index}>
+              <ListItem>
+                <ListItemText primary={provider} />
+              </ListItem>
+              <Divider component="li" />
+            </React.Fragment>
+          ))}
+        </List>
       </Grid>
       <Grid item xs style={{ flexGrow: 2.5 }}>
         <ProviderMap></ProviderMap>
       </Grid>
       <Grid item xs style={{ flexGrow: 1 }}>
-        <p>Alert</p>
+        <p>Alerts</p>
       </Grid>
     </Grid>
   );
