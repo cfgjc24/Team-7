127b1712d280698c002e9a9e82bafd1ea729c94b
Amit Howlader <amithowlader@Amits-MacBook-Air.local>
2024-09-20 23:12:03 -0400

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
-from flask import Flask
-from . import db
+from flask import Flask, request, jsonify
+from .mongo import save
 
 app = Flask(__name__, static_url_path="", static_folder="../build")
+
+@app.route("/clockIn", methods=['POST'])
+def clockIn():
+    data = request.get_json()
+    save(data['caregiverID'], data['state'], data['geodata'], data['client'], data['timestamp'], data['active'])
+    return jsonify({'message': 'Clocked In'})
