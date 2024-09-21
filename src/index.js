2a0af735e1de26b45a53eb94721dbcaea0bd8671
taisnguyen <tai.sanh.nguyen@fusionsystems.co.jp>
2024-09-21 01:50:40 -0400

    added login page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom/client";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import ClockInOut from "./pages/ClockInPage/clockin.js"; /*Automatically loads in page*/
 import LoginSignup from "./pages/signin/LoginSignup/LoginSignup.jsx";
-const ProviderMap = React.lazy(() => import("./pages/ProviderMap/supervisor.js"));
+const ProviderMap = React.lazy(() =>
+  import("./pages/ProviderMap/supervisor.js")
+);
 // const ClockInPage = React.lazy(() => import("./pages/ClockInPage/clockin.js"));
 const Home = React.lazy(() => import("./pages/Home/home.js"));
 
@@ -20,7 +22,10 @@ root.render(
         <Route path="/" element={<Home />} />
         <Route path="/clock-in" element={<ClockInOut />} />
         <Route path="/ProviderMap" element={<ProviderMap />} />
-        <Route path="/login" element={<LoginSignup />} />
+        <Route
+          path="/login"
+          element={<Navigate to="http://localhost:5000/login" />}
+        />
       </Routes>
     </BrowserRouter>
   </Layout>
