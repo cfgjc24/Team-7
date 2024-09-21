20d8d509397993be09fc6f51236ff3b2ec7994d5
Arielle Dong <112361805+Arielle8219@users.noreply.github.com>
2024-09-20 20:22:36 -0400

    added provider lists

---
+++ src/pages/ProviderMap/provider.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Box,
+  TextField,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+const Provider = () => {
+  const [providerList, setProviderList] = useState([]);
+
+  useEffect(() => {
+    setProviderList(["provider 1", "provider 2"]);
+  }, []);
+
+  return (
+    <Container>
+      <p>Find Provider</p>
+      <TextField
+        id="search-provider"
+        label="Search by name"
+        variant="outlined"
+      />
+      <hr></hr>
+      <List>
+        {providerList.map((provider, index) => (
+          <React.Fragment key={index}>
+            <ListItem>
+              <ListItemText primary={provider} />
+            </ListItem>
+            <Divider component="li" />
+          </React.Fragment>
+        ))}
+      </List>
+    </Container>
+  );
+};
+export default Provider;
--- src/pages/ProviderMap/supervisor.js
+++ src/pages/ProviderMap/supervisor.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ProviderMap from "./map";
+import Provider from "./provider";
 import {
   Container,
   Box,
@@ -11,32 +12,10 @@ import {
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 const Supervisor = () => {
-  const [providerList, setProviderList] = useState([]);
-
-  useEffect(() => {
-    setProviderList(["provider 1", "provider 2"]);
-  }, []);
-
   return (
     <Grid container>
       <Grid item xs style={{ flexGrow: 0.5 }}>
-        <p>Find Provider</p>
-        <TextField
-          id="search-provider"
-          label="Search by name"
-          variant="outlined"
-        />
-        <hr></hr>
-        <List>
-          {providerList.map((provider, index) => (
-            <React.Fragment key={index}>
-              <ListItem>
-                <ListItemText primary={provider} />
-              </ListItem>
-              <Divider component="li" />
-            </React.Fragment>
-          ))}
-        </List>
+        <Provider></Provider>
       </Grid>
       <Grid item xs style={{ flexGrow: 2.5 }}>
         <ProviderMap></ProviderMap>
