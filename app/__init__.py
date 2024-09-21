40f6e126dfd0b5a653f26bdafd5eeca4c9969be5
Amit Howlader <amithowlader@Amits-MacBook-Air.local>
2024-09-21 00:53:42 -0400

    getActive

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from .mongo import save
+from .mongo import save, get
 
 app = Flask(__name__, static_url_path="", static_folder="../build")
 
@@ -8,3 +8,13 @@ def clockIn():
     data = request.get_json()
     save(data['caregiverID'], data['state'], data['geodata'], data['client'], data['timestamp'], data['active'])
     return jsonify({'message': 'Clocked In'})
+
+
+@app.route("/getActive", methods=['GET'])
+def getActive():
+    data = get().get_json()
+
+    print(type(data))
+    print(type(jsonify(data)))
+    
+    return 'success'
