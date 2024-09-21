31c898a7aaf8bc3f42ab1bb34e289a6ea8be5a59
Arielle Dong <112361805+Arielle8219@users.noreply.github.com>
2024-09-21 02:52:51 -0400

    changing ui for home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -7,26 +7,48 @@ const HomePage = () => {
   return (
     <Container maxWidth="sm">
       <Box textAlign="center" mt={5}>
-        <Typography variant="h4" gutterBottom>
+        {/*         <Typography variant="h4" gutterBottom>
           LodeWatch
-        </Typography>
+        </Typography> */}
         <img
           src="lodestarlogo.png"
           style={{ width: "500px", height: "auto" }}
         />
-        <Typography variant="body1" gutterBottom>
+        {/*         <Typography variant="body1" gutterBottom>
           Sign In Below
-        </Typography>
-        {/* EDIT for Sign In Button  */}
-        <Button variant="contained" color="primary" onClick={() => navigate("/signin")}>
-          Sign In Page
+        </Typography> */}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            window.location.href = "http://localhost:5000/login";
+          }}
+        >
+          Sign In
         </Button>
 
-        <Typography variant="body1" gutterBottom>
+        {/*         <Typography variant="body1" gutterBottom>
           Clock In Below
         </Typography>
-     {/* EDIT for Supervisor Dashboard */}
-        <Button variant="contained" color="primary" onClick={() => navigate("/ProviderMap")}>
+ */}
+        {/* Use Link to refer to the clock page */}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/clock-in")}
+        >
+          Clock In/Out
+        </Button>
+        {/* 
+        <Typography variant="body1" gutterBottom>
+          Supervisor Dashboard
+        </Typography> */}
+        {/* EDIT for Supervisor Dashboard */}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate("/clock-in")}
+        >
           Supervisor Dashboard
         </Button>
       </Box>
